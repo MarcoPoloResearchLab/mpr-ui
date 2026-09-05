243363281557417c964378acb6e1dd3c4e056bc1
Vadym Tyemirov <temirov@gmail.com>
2026-09-05 12:57:14 -0700

    chore: add Pinguin bootstrap env vars to down.sh

--- down.sh
+++ down.sh
@@ -2,6 +2,9 @@ TAUTH_JWT_SIGNING_KEY=down \
 PINGUIN_DEMO_API_KEY=down \
 PINGUIN_DEMO_CREDENTIAL_ID=down \
 PINGUIN_DEMO_CREDENTIAL_DIGEST=down \
+PINGUIN_BOOTSTRAP_PASSWORD=down \
+PINGUIN_BOOTSTRAP_PASSWORD_HASH=down \
+PINGUIN_BOOTSTRAP_SIGNING_KEY=down \
 docker compose \
   --env-file demo/.env.tauth \
   --env-file ../Pinguin/configs/.env.pinguin \
